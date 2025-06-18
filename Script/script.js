9d02402fe0415e8db08fce9d7b6e2466a3ce731d
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-06-18 20:39:15 +0100

    Local Storage is Done

--- Script/script.js
+++ Script/script.js
@@ -3,7 +3,7 @@ const handleAddToCart = () => {
   const productQuantity = document.getElementById("productQuantity");
   let name = productName.value;
   let quantity = productQuantity.value;
-  saveProductToLocalStorage(name,quantity);
+  saveProductToLocalStorage(name, quantity);
   displayCart(name, quantity);
   productName.value = "";
   productQuantity.value = "";
@@ -15,16 +15,32 @@ const displayCart = (name, quantity) => {
   list.innerText = `${name}:${quantity}`;
   productList.appendChild(list);
 };
-const getProductFromLocalStorage=()=>{
+const getProductFromLocalStorage = () => {
   let cart = {};
   const getProduct = localStorage.getItem("cart");
-  if(getProduct){
+  if (getProduct) {
     cart = JSON.parse(getProduct);
   }
   return cart;
-
-}
-
-const saveProductToLocalStorage = (name,quantity) => {
-  let cart = getProductFromLocalStorage();
 };
+
+const displayProductFromLocalStorage = () => {
+
+  const products = getProductFromLocalStorage();
+  console.log(products);
+  for (let product in products) {
+    console.log(product);
+    console.log(products[product]);
+    displayCart(product,products[product]);
+
+  }
+};
+
+const saveProductToLocalStorage = (name, quantity) => {
+  let cart = getProductFromLocalStorage();
+  cart[name] = quantity;
+
+  const stringCart = JSON.stringify(cart);
+  localStorage.setItem("cart", stringCart);
+};
+displayProductFromLocalStorage();
